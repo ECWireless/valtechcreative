9c94d07e405d12c8db181b5d3e7e747ee7e629df
ECWireless <Elliott@valtechcreative.com>
2020-07-09 15:42:35 -0600

    First Draft. No responsiveness...

--- components/index.js
+++ components/index.js
@@ -1,10 +1,46 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+export const Card = styled.div`
+    position: absolute;
+    bottom: 50%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    width: 85vw;
+    height: 85vh;
+    background: #fff;
+    border-radius: 15px;
+    -webkit-box-shadow: 1px 5px 5px -1px rgba(0,0,0,0.38);
+    -moz-box-shadow: 1px 5px 5px -1px rgba(0,0,0,0.38);
+    box-shadow: 1px 5px 5px -1px rgba(0,0,0,0.38);
+    border: 2px solid #FBF30A;
+`
 
 export const Container = styled.div`
     width: 900px;
-    margin: 0 auto;
+    margin: auto;
 `
 
 export const H1 = styled.h1`
-    font-size: 32px;
+    font-size: 45px;
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: 500;
+
+    ${props =>
+        props.uppercase &&
+        css`
+            text-transform: uppercase;
+        `};
+`
+
+export const P = styled.p`
+    font-size: 18px;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+    color: #094c89;
+
+    ${props =>
+        props.uppercase &&
+        css`
+            text-transform: uppercase;
+        `};
 `
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,29 @@ import Head from 'next/head';
 
 // Components
 import {
+    Card,
     Container,
     H1,
+    P,
 } from '../components'
 
 const index = () => {
     return (
         <>
             <Head>
-                <title>Valtech Creative</title>
+                <title>Valtech Creative | Under</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
             </Head>
             <main>
-                <Container style={{marginTop: '100px', textAlign: 'center'}}>
-                    <H1>The Valtech website is currently being updated. Thanks for your patients!</H1>
-                </Container>
+                <Card>
+                    <Container style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+                        <P style={{fontSize: '24px'}}>The</P>
+                        <H1 style={{marginTop: '15px'}}>Valtech website</H1>
+                        <P style={{fontSize: '24px', marginTop: '25px'}}>is currently being updated. Thanks for your patients!</P>
+                        <img style={{width: '450px', height: '350px', marginTop: '40px'}} src="/routine-maintenance.gif" alt="working"/>
+                    </Container>
+                </Card>
             </main>
         </>
     )
