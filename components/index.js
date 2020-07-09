16ed7c3bfd6a0165e805232e8ebc8712b43b7ae5
ECWireless <Elliott@valtechcreative.com>
2020-07-08 21:48:08 -0600

    Experiment with Style-Components

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 900px;
+    margin: 0 auto;
+`
+
+export const H1 = styled.h1`
+    font-size: 32px;
+`
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import Head from 'next/head';
+
+// Components
+import {
+    Container,
+    H1,
+} from '../components'
+
 const index = () => {
     return (
-        <div>
-            Website is currently being updated. Thanks for your patients!
-        </div>
+        <>
+            <Head>
+                <title>Valtech Creative</title>
+            </Head>
+            <main>
+                <Container style={{marginTop: '100px', textAlign: 'center'}}>
+                    <H1>The Valtech website is currently being updated. Thanks for your patients!</H1>
+                </Container>
+            </main>
+        </>
     )
 }
 
