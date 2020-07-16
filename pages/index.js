4ea6fc87899c864813fe992aa06af2b276599865
ECWireless <Elliott@valtechcreative.com>
2020-07-16 16:24:31 -0600

    Finish Desktop Hero

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import Head from 'next/head';
+import Head from 'next/head'
+import Particles from 'react-particles-js'
+import ReactRotatingText from 'react-rotating-text'
 
 // Components
 import {
@@ -22,12 +24,42 @@ const index = () => {
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
             </Head>
             <main>
+                <Particles
+                    id="particles"
+                    params={{
+                        "particles": {
+                            "number": {
+                                "value": 100
+                            },
+                            "size": {
+                                "value": 3
+                            },
+                            "color": {
+                                "value": "#1263AD"
+                            },
+                            "line_linked": {
+                                "enable": true,
+                                "color": "#1263AD"
+                            },
+                        },
+                        "interactivity": {
+                            "events": {
+                                "onhover": {
+                                    "enable": true,
+                                    "mode": "repulse"
+                                }
+                            }
+                        }
+                    }}
+                />
                 <Container>
                     <H1 id="hero__heading1">A Collaborative Agency for UX/UI Design</H1>
                     <Flex>
                         <H2 id="hero__heading2">Based in...  </H2>
                         <LocationIcon />
-                        <H2 id="hero__heading2">Anywhere</H2>
+                        <H2 id="hero__location"><ReactRotatingText
+                            items={['Los Angeles', 'Pittsburgh', 'Amsterdam', 'Boulder', 'Anywhere']}
+                        /></H2>
                     </Flex>
                     <P1 id="hero__paragraph1">Specializing in custom Websites, Web Apps, and Native Applications</P1>
                     
