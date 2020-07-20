0302c6a2814450270d440cfb6a850f0053ff92eb
ECWireless <Elliott@valtechcreative.com>
2020-07-20 16:31:21 -0600

    Remove class Component

--- containers/Features.js
+++ containers/Features.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 // Components
 import {
     Card,
@@ -16,85 +14,77 @@ import CustomizationIcon from '../components/CustomizationIcon'
 import SearchIcon from '../components/SearchIcon'
 import SpeedIcon from '../components/SpeedIcon'
 
-export default class Features extends React.Component {
-    state = {
-        loading: true,
-    }
-
-    componentDidMount() {
-        this.setState({ loading: false })
-    }
-
-    render() {
-        return (
-            <>
-                <Transform y={-200}>
-                    <Container>
-                        <Card width={100} paddingTop={0} paddingBottom={150}>
-                            <Col2>
-                                <Col2Left align={'center'} justify={'flex-start'}>
-                                    <Box2 width={50} marginTop={150}>
-                                        {!this.state.loading && <CustomizationIcon />}
-                                    </Box2>
-                                    <Box3 with={300} marginTop={40}>
-                                        <H3 align={'center'}>Complete<br/>Customization</H3>
-                                        <Box3 marginTop={20}>
-                                            <Line width={100} height={5} color={'yellow'} />
-                                        </Box3>
+const Features = () => {
+    return (
+        <>
+            <Transform y={-200}>
+                <Container>
+                    <Card width={100} paddingTop={0} paddingBottom={150}>
+                        <Col2>
+                            <Col2Left align={'center'} justify={'flex-start'}>
+                                <Box2 width={50} marginTop={150}>
+                                    <CustomizationIcon />
+                                </Box2>
+                                <Box3 with={300} marginTop={40}>
+                                    <H3 align={'center'}>Complete<br/>Customization</H3>
+                                    <Box3 marginTop={20}>
+                                        <Line width={100} height={5} color={'yellow'} />
                                     </Box3>
-                                    <Box3 width={360} marginTop={30}>
-                                        <P2 align={'center'}>We work with our clients 1-on-1 to get your branding just right.</P2>
+                                </Box3>
+                                <Box3 width={360} marginTop={30}>
+                                    <P2 align={'center'}>We work with our clients 1-on-1 to get your branding just right.</P2>
+                                </Box3>
+                            </Col2Left>
+                            <Col2Right align={'center'} justify={'flex-start'}>
+                                <Box2 width={50} marginTop={150}>
+                                    <SpeedIcon />
+                                </Box2>
+                                <Box3 width={300} marginTop={40}>
+                                    <H3 align={'center'}>Blazing<br/>Load Speeds</H3>
+                                    <Box3 marginTop={20}>
+                                        <Line width={100} height={5} color={'yellow'} />
                                     </Box3>
-                                </Col2Left>
-                                <Col2Right align={'center'} justify={'flex-start'}>
-                                    <Box2 width={50} marginTop={150}>
-                                        {!this.state.loading && <SpeedIcon />}
-                                    </Box2>
-                                    <Box3 width={300} marginTop={40}>
-                                        <H3 align={'center'}>Blazing<br/>Load Speeds</H3>
-                                        <Box3 marginTop={20}>
-                                            <Line width={100} height={5} color={'yellow'} />
-                                        </Box3>
+                                </Box3>
+                                <Box3 width={360} marginTop={30}>
+                                    <P2 align={'center'}>Our frameworks are modern, flexible, and always fast.</P2>
+                                </Box3>
+                            </Col2Right>
+                        </Col2>
+                        <Col2>
+                            <Col2Left align={'center'} justify={'flex-start'}>
+                                <Box2 width={50} marginTop={150}>
+                                    <BackendIcon />
+                                </Box2>
+                                <Box3 with={300} marginTop={40}>
+                                    <H3 align={'center'}>Backend<br/>Infrastructure</H3>
+                                    <Box3 marginTop={20}>
+                                        <Line width={100} height={5} color={'yellow'} />
                                     </Box3>
-                                    <Box3 width={360} marginTop={30}>
-                                        <P2 align={'center'}>Our frameworks are modern, flexible, and always fast.</P2>
+                                </Box3>
+                                <Box3 width={350} marginTop={30}>
+                                    <P2 align={'center'}>Custom CMS configuration is available for any projects that need consistently updated or maintained content</P2>
+                                </Box3>
+                            </Col2Left>
+                            <Col2Right align={'center'} justify={'flex-start'}>
+                                <Box2 width={50} marginTop={150}>
+                                    <SearchIcon />
+                                </Box2>
+                                <Box3 width={300} marginTop={40}>
+                                    <H3 align={'center'}>SEO<br/>Friendly</H3>
+                                    <Box3 marginTop={20}>
+                                        <Line width={100} height={5} color={'yellow'} />
                                     </Box3>
-                                </Col2Right>
-                            </Col2>
-                            <Col2>
-                                <Col2Left align={'center'} justify={'flex-start'}>
-                                    <Box2 width={50} marginTop={150}>
-                                        {!this.state.loading && <BackendIcon />}
-                                    </Box2>
-                                    <Box3 with={300} marginTop={40}>
-                                        <H3 align={'center'}>Backend<br/>Infrastructure</H3>
-                                        <Box3 marginTop={20}>
-                                            <Line width={100} height={5} color={'yellow'} />
-                                        </Box3>
-                                    </Box3>
-                                    <Box3 width={350} marginTop={30}>
-                                        <P2 align={'center'}>Custom CMS configuration is available for any projects that need consistently updated or maintained content</P2>
-                                    </Box3>
-                                </Col2Left>
-                                <Col2Right align={'center'} justify={'flex-start'}>
-                                    <Box2 width={50} marginTop={150}>
-                                        {!this.state.loading && <SearchIcon />}
-                                    </Box2>
-                                    <Box3 width={300} marginTop={40}>
-                                        <H3 align={'center'}>SEO<br/>Friendly</H3>
-                                        <Box3 marginTop={20}>
-                                            <Line width={100} height={5} color={'yellow'} />
-                                        </Box3>
-                                    </Box3>
-                                    <Box3 width={350} marginTop={30}>
-                                        <P2 align={'center'}>Our websites are never shipped without a 100% score from Google Lighthouse.</P2>
-                                    </Box3>
-                                </Col2Right>
-                            </Col2>
-                        </Card>
-                    </Container>
-                </Transform>
-            </>
-        )
-    }
+                                </Box3>
+                                <Box3 width={350} marginTop={30}>
+                                    <P2 align={'center'}>Our websites are never shipped without a 100% score from Google Lighthouse.</P2>
+                                </Box3>
+                            </Col2Right>
+                        </Col2>
+                    </Card>
+                </Container>
+            </Transform>
+        </>
+    )
 }
+
+export default Features
--- containers/Hero.js
+++ containers/Hero.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Particles from 'react-particles-js'
 import ReactRotatingText from 'react-rotating-text'
 
@@ -8,66 +7,58 @@ import { H1, H2, P1 } from '../components/Typography'
 import { Container, Flex } from '../components/Containers'
 import LocationIcon from '../components/LocationIcon'
 
-export default class Hero extends React.Component {
-    state = {
-        loading: true,
-    }
-
-    componentDidMount() {
-        this.setState({ loading: false })
-    }
-
-    render() {
-        return (
-            <>
-                <Particles
-                    id="particles"
-                    params={{
-                        "particles": {
-                            "number": {
-                                "value": 100
-                            },
-                            "size": {
-                                "value": 3
-                            },
-                            "color": {
-                                "value": "#1263AD"
-                            },
-                            "line_linked": {
-                                "enable": true,
-                                "color": "#1263AD"
-                            },
+const Hero = () => {
+    return (
+        <>
+            <Particles
+                id="particles"
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 100
                         },
-                        "interactivity": {
-                            "events": {
-                                "onhover": {
-                                    "enable": true,
-                                    "mode": "repulse"
-                                }
+                        "size": {
+                            "value": 3
+                        },
+                        "color": {
+                            "value": "#1263AD"
+                        },
+                        "line_linked": {
+                            "enable": true,
+                            "color": "#1263AD"
+                        },
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
                             }
                         }
-                    }}
-                />
-                <Container>
-                    <Box width={800} marginTop={150}>
-                        <H1>A Collaborative Agency for UX/UI Design</H1>
-                    </Box>
-                    <Flex>
-                        <Box marginTop={60}>
-                            <H2 id="hero__heading2">Based in...</H2>
-                        </Box>
-                        {!this.state.loading && <LocationIcon />}
-                        <Box marginTop={60}>
-                            <H2 id="hero__location"><ReactRotatingText
-                                items={['Los Angeles', 'Pittsburgh', 'Amsterdam', 'Boulder', 'Anywhere']}
-                            /></H2>
-                        </Box>
-                    </Flex>
+                    }
+                }}
+            />
+            <Container>
+                <Box width={800} marginTop={150}>
+                    <H1>A Collaborative Agency for UX/UI Design</H1>
+                </Box>
+                <Flex>
                     <Box marginTop={60}>
-                        <P1 id="hero__paragraph1">Specializing in custom Websites, Web Apps, and Native Applications</P1>
+                        <H2 id="hero__heading2">Based in...</H2>
                     </Box>
-                </Container>
-            </>
-        )
-    }
+                        <LocationIcon />
+                    <Box marginTop={60}>
+                        <H2 id="hero__location"><ReactRotatingText
+                            items={['Los Angeles', 'Pittsburgh', 'Amsterdam', 'Boulder', 'Anywhere']}
+                        /></H2>
+                    </Box>
+                </Flex>
+                <Box marginTop={60}>
+                    <P1 id="hero__paragraph1">Specializing in custom Websites, Web Apps, and Native Applications</P1>
+                </Box>
+            </Container>
+        </>
+    )
 }
+
+export default Hero
