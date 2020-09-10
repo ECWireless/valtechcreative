c14b3bdc8164ec0ced2a36d632489a19bd4cd684
ECWireless <Elliott@valtechcreative.com>
2020-09-10 17:04:16 -0600

    Add Google Analytics Tag

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,24 @@ const index = () => {
 
                 <link rel="icon" href="/favicon.ico" />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=UA-111622721-4"
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+                        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111622721-4"></script>
+                        <script>
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                        
+                            gtag('config', 'UA-111622721-4');
+                        </script>
+                        `,
+                    }}
+                />
             </Head>
             <main>
                 <NavBar setClosed={setClosed} />
