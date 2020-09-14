46f48f3515ed1cfb9fd0ef019ed9ed869d7d44c9
ECWireless <Elliott@valtechcreative.com>
2020-09-14 13:53:08 -0600

    Fix Lighthouse errors

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import NextDocument from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from 'styled-components'
-export default class Document extends NextDocument {
-  static async getInitialProps (ctx) {
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
+
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
         })
-      const initialProps = await NextDocument.getInitialProps(ctx)
+
+      const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
@@ -18,10 +21,36 @@ export default class Document extends NextDocument {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
+        ),
       }
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-111622721-4" >
+        </script>
+        <script dangerouslySetInnerHTML={
+            { __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments)}
+                gtag("js", new Date());
+                gtag("config", "UA-111622721-4");
+            `}
+        }>
+        </script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
