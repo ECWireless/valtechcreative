7e563d6c2d72b40c93a34030199c2ac24145bb81
ECWireless <Elliott@valtechcreative.com>
2020-09-25 09:56:55 -0600

    Remove polyfill

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from 'styled-components'
-import "scroll-behavior-polyfill";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
