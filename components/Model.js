dad5ed410f8131861eff4d87ffcfd84a4901ed7b
ECWireless <Elliott@valtechcreative.com>
2020-09-24 22:00:28 -0600

    Start new model

--- components/Model.js
+++ components/Model.js
@@ -1,13 +1,110 @@
-import styled from 'styled-components'
-import Fade from 'react-reveal/Fade'
+import { useState } from 'react'
+import styled, { css } from 'styled-components'
+import Fade from 'react-reveal/Fade';
+
+// Components
+import { colors } from '../components/theme'
+import { Button } from '../components/Buttons'
+import { Container, Flex } from '../components/Containers'
+import { Form, Input, Label, TextArea } from '../components/Forms'
+import { H3 } from '../components/Typography'
+import { Box } from '../components/Boxes'
+import Snackbar from '../components/Snackbar'
+import Spinner from '../components/Spinner'
 
 const Model = () => {
+
+    const [status, setStatus] = useState({
+        submitted: false,
+        submitting: false,
+        info: { error: false, msg: null }
+    })
+    
+    const [inputs, setInputs] = useState({
+        email: '',
+        message: ''
+    })
+
+    const [snackbar, setSnackbar] = useState(false)
+
+    const handleResponse = (status, msg) => {
+        if (status === 200) {
+            setStatus({
+                submitted: true,
+                submitting: false,
+                info: { error: false, msg: msg }
+            })
+            setInputs({
+                name: '',
+                email: '',
+                phone: '',
+                message: ''
+            })
+            setSnackbar(true)
+        } else {
+            setStatus({
+                info: { error: true, msg: msg }
+            })
+            setSnackbar(true)
+        }
+    }
+
+    const handleOnChange = e => {
+        e.persist()
+        setInputs(prev => ({
+            ...prev,
+            [e.target.id]: e.target.value
+        }))
+        setStatus({
+            submitted: false,
+            submitting: false,
+            info: { error: false, msg: null }
+        })
+    }
+
+    const handleOnSubmit = async e => {
+        e.preventDefault()
+        setStatus(prevStatus => ({ ...prevStatus, submitting: true }))
+        const res = await fetch('/api/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(inputs)
+        })
+        const text = await res.text()
+        handleResponse(res.status, text)
+    }
+
+    const onCloseSnackbar = () => {
+        setSnackbar(false)
+    }
+
     return (
-        <ModelContainer>
-            <Fade>
-                <iframe width="600px" height="550px" frameBorder="none" src="https://cdn.forms-content.sg-form.com/1a3e6333-fedf-11ea-a4f1-0ab38a2c3a50"/>
-            </Fade>
-        </ModelContainer>
+        <>
+            <ModelContainer>
+                <Container>
+                    <Form style={{ background: colors.white }} paddingTop={50} paddingBottom={50} onSubmit={handleOnSubmit}>
+                        <Box marginBottom={50}>
+                            <H3 color={'#000'} align={'center'}>Connect with a Developer</H3>
+                        </Box>
+                        <Label htmlFor="name">Name:</Label>
+                        <Input required marginTop={10} type="text" id="name" onChange={handleOnChange} value={inputs.name} />
+                        <Label marginTop={50} htmlFor="email">Email:</Label>
+                        <Input required marginTop={10} type="email" id="email" onChange={handleOnChange} value={inputs.email} />
+                        <Button marginTop={50} type="submit">
+                            {!status.submitting
+                                ? !status.submitted
+                                ? 'Submit'
+                                : 'Submitted'
+                                : <Spinner />
+                            }
+                        </Button>
+                    </Form>
+                </Container>
+            </ModelContainer>
+            { snackbar && <Snackbar success={!status.info.error} onCloseSnackbar={onCloseSnackbar} /> }
+        </>
     )
 }
 
@@ -15,8 +112,8 @@ export default Model
 
 const ModelContainer = styled.div`
     position: fixed;
-    z-index: 1000;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    z-index: 1000;
 `
